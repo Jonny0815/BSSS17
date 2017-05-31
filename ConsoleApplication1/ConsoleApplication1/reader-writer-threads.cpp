6f5f7abe6a7dc6e5f530d0f31f52afaba467e567
Jonny0815 <Jonny0815@JONNY0815>
2017-05-31 17:34:46 +0200

    fair solution !

--- ConsoleApplication1/ConsoleApplication1/reader-writer-threads.cpp
+++ ConsoleApplication1/ConsoleApplication1/reader-writer-threads.cpp
@@ -1,7 +1,7 @@
 #include <iostream>			// for std::cin, cerr, cout ...
 #include <thread>			// for std::this_thread
 #include <chrono>			// for std::chrono... 
-
+#include <mutex>
 #include "database.h"
 #include "reader-writer-threads.h"
 
@@ -9,16 +9,47 @@
 // ******** reader & writer threads ******** 
 
 // The writer thread
+
+int rc = 0;
+
+std::mutex mtx_enter;
+std::mutex mtx_exit;
+std::mutex mtx_rc;
+
+
 void writer( int writerID, int numSeconds ) {
 
 	std::cout << "Writer " << writerID << " starting..." << std::endl;
 
 	int	tests = 0; 
+	
 	auto startTime = std::chrono::steady_clock::now();
 	std::chrono::seconds maxTime( numSeconds ); 
 	while ( ( std::chrono::steady_clock::now() - startTime ) < maxTime ) {
 		
+		mtx_enter.lock();
+		
+		while (true)
+		{
+			mtx_rc.lock();
+
+			if (rc == 0)
+			{
+				mtx_rc.unlock();
+				break;
+			}
+
+			mtx_rc.unlock();
+			std::cout << "Writer " << writerID << " sleeping for 100ms ..." << std::endl;
+			std::this_thread::sleep_for(std::chrono::milliseconds(100));
+			
+		}
+		
+
 		bool result = theDatabase.write( writerID );
+
+		mtx_enter.unlock();
+
 		++tests;
 
 		// sleep a while...
@@ -49,7 +80,21 @@ void reader( int readerID, int numSeconds ) {
 	std::chrono::seconds maxTime( numSeconds ); 
 	while ( ( std::chrono::steady_clock::now() - startTime ) < maxTime ) {
 
+
+		mtx_enter.lock();
+		mtx_rc.lock();
+		rc++;
+		mtx_rc.unlock();
+		mtx_enter.unlock();
+
 		bool result = theDatabase.read( readerID );
+
+		mtx_exit.lock();
+		mtx_rc.lock();
+		rc--;
+		mtx_rc.unlock();
+		mtx_exit.unlock();
+
 		++tests;
 
 		// sleep a while...
